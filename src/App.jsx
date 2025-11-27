e6b00021c6f0c171082a95409e191662ca0af02b
damir-ilyasov <damir2006ilyasov@gmail.com>
2025-11-27 13:10:31 +0300

    Calculator_KR4

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import BMICalculator from "./components/BMICalculator"
+import "./App.css"
+
+function App() {
+  return (
+    <div className="app">
+      <BMICalculator />
+    </div>
+  )
+}
+
+export default App
